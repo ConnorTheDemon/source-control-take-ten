2249f9e86eba6f930740c8f8e091c34bf69eb2a7
Connor Williams <connorhakan8@gmail.com>
2022-09-12 17:47:00 -0700

    Initial Commit

---
+++ source control take ten/source_control_take_tenApp.swift
@@ -0,0 +1,17 @@
+//
+//  source_control_take_tenApp.swift
+//  source control take ten
+//
+//  Created by Connor on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct source_control_take_tenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
